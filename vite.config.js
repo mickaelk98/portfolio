cbaa54a951a23f690b97649bd87041016485a79c
mickael keita <keitamickael@gmail.com>
2022-11-21 16:49:29 +0100

    configuration de vite pour utiliser les variables d'environnement avec netlify

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: viteEnv,
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    // alias: {
+    //   "@": require("path").resolve(__dirname, "src"),
+    // },
+  },
+});
