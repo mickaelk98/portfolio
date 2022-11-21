0fd18c27fff6552833dc5aa7f21ff33dfd380c81
mickael keita <keitamickael@gmail.com>
2022-11-21 12:45:41 +0100

    traitement des emails avec emailjs

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,4 +1,62 @@
 <script setup>
+import { useField, useForm } from 'vee-validate';
+import { z } from 'zod'
+import { toFormValidator } from '@vee-validate/zod'
+import emailjs from '@emailjs/browser';
+
+// verification des saisies de l'utilisateur
+const validationSchema = toFormValidator(
+  z.object({
+    name: z.string({ required_error: "Ce champ est obligatoire" })
+      .regex(new RegExp("^([a-z]+(( |')[a-z]+)*)+([-]([a-z]+(( |')[a-z]+)*)+)*$"), "Votre nom nest pas conforme")
+      .max(50, "votre nom doit faire moin de 50 caractères"),
+    email: z.string({ required_error: "Ce champ est obligatoire" })
+      .email("Vous devez rentrez une adresse email"),
+    subject: z.string({ required_error: "Ce champ est obligatoire" })
+      .min(1, "Ce champ ne peut pas etre vide")
+      .max(100, "Votre sujet ne doit pas depasser 100 caractères"),
+    message: z.string({ required_error: "Ce champ est obligatoire" })
+      .min(1, "Ce champ ne peut pas etre vide")
+      .max(1000, "Votre message doit faire maximum de 1000 caractères"),
+  })
+)
+
+// acceder au données a la soumission du formulaire
+const { handleSubmit, setErrors } = useForm({ validationSchema })
+
+
+// capture des saisies de l'utilisateur
+const { value: nameValue, errorMessage: nameError } = useField("name");
+const { value: emailValue, errorMessage: emailError } = useField("email")
+const { value: subjectValue, errorMessage: subjectError } = useField("subject")
+const { value: messageValue, errorMessage: messageError } = useField("message")
+
+
+// fonction pour soummettre un formulaire
+const sendMessage = handleSubmit(async (formValue, { resetForm }) => {
+  try {
+    const templateParams = {
+      name: formValue.name,
+      email: formValue.email,
+      subject: formValue.subject,
+      message: formValue.message
+    };
+
+    const templateID = "template_s55k1eg"
+    const serviceID = "service_nkh4tr2"
+    const publicKey = `${import.meta.env.VITE_EMAILJS_PUBLICKEY}`
+
+    const result = await emailjs.send(serviceID, templateID, templateParams, publicKey);
+
+    if (result.status === 200) {
+      resetForm()
+      alert('Votre message a bien été envoyé')
+    }
+
+  } catch (e) {
+    console.log(e);
+  }
+})
 
 </script>
 
@@ -9,10 +67,23 @@
         <h2>contact</h2>
         <p>N'hésitez pas à me contacter, je vous répondrai le plus tôt possible.</p>
       </div>
-      <form name="contact" method="post" netlify>
-        <input type="text" name="nom" placeholder="Nom">
-        <input type="text" name="email" placeholder="Email">
-        <textarea name="message" placeholder="Votre message"></textarea>
+      <form @submit="sendMessage">
+        <!-- nom -->
+        <input v-model="nameValue" type="text" placeholder="Nom">
+        <small v-if="nameError">{{ nameError }}</small>
+
+        <!-- email -->
+        <input v-model="emailValue" type="text" placeholder="Email">
+        <small v-if="emailError">{{ emailError }}</small>
+
+        <!-- sujet -->
+        <input v-model="subjectValue" type="text" placeholder="Le sujet de votre message">
+        <small v-if="subjectError">{{ subjectError }}</small>
+
+        <!-- message -->
+        <textarea v-model="messageValue" placeholder="Votre message"></textarea>
+        <small v-if="messageError">{{ messageError }}</small>
+
         <button class="btn">Envoyer</button>
       </form>
     </div>
